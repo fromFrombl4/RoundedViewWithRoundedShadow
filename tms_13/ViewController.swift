937f83afd07c162b2581d59f58001b85049cb641
roman dod <stalkeroma1@mail.ru>
2020-08-03 20:03:29 +0300

    Initial Commit

---
+++ tms_13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tms_13
+//
+//  Created by Roman Dod on 8/3/20.
+//  Copyright © 2020 Roman Dod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
